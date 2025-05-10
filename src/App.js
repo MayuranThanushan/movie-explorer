39a8aee1aa96a10f974bc0b308382d6136c46d6c
MayuranThanushan <mayuranthanushan@gmail.com>
2025-05-11 03:35:32 +0530

    implement 'Load More' button for search results

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import Favorites from "./pages/Favorites";
 import MovieDetails from "./pages/MovieDetails";
 import Navbar from "./components/Navbar";
 import { Routes, Route } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
+import SearchResults from "./pages/SearchResults";
+
 function App() {
+  const [searchParams] = useSearchParams();
+  const query = searchParams.get("q");
+    
   return (
     <>
     <Navbar />
@@ -12,6 +18,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/movie/:id" element={<MovieDetails />} />
         <Route path="/favorites" element={<Favorites />} />
+        <Route path="/search" element={<SearchResults query={query} />} />
       </Routes>
     </>
   );
